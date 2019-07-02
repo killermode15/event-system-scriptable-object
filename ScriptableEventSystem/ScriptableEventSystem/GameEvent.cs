c3f7d7b03173f649eb06c1346206903873eb2c31
Sean Keiffer Lee <seankeiffer@gmail.com>
2019-07-02 12:25:35 +0800

    + Initial Push

---
+++ ScriptableEventSystem/ScriptableEventSystem/GameEvent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ScriptableEventSystem
+{
+    [CreateAssetMenu]
+    public class GameEvent : ScriptableObject
+    {
+        private List<GameEventListener> listeners = new List<GameEventListener>();
+
+        public void Raise()
+        {
+            foreach (GameEventListener listener in listeners)
+            {
+                listener.OnEventRaised();
+            }
+        }
+
+        public void RegisterListener(GameEventListener listener)
+        {
+            listeners.Add(listener);
+        }
+
+        public void UnregisterListener(GameEventListener listener)
+        {
+            listeners.Remove(listener);
+        }
+
+    }
+}
---
+++ ScriptableEventSystem/ScriptableEventSystem/GameEventListener.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace ScriptableEventSystem
+{
+    public class GameEventListener : MonoBehaviour
+    {
+        [SerializeField] private GameEvent gameEvent;
+        [SerializeField] private UnityEvent response;
+
+        private void OnEnable()
+        {
+            gameEvent.RegisterListener(this);
+        }
+
+        private void OnDisable()
+        {
+            gameEvent.UnregisterListener(this);
+        }
+
+        public void OnEventRaised()
+        {
+            response.Invoke();
+        }
+
+    }
+}
